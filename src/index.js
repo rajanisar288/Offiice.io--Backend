db12b1b5c6bf80bc88dc5441cb0a17d82e25e1ec
rajanisar288 <raaji4194@gmail.com>
2023-12-22 18:45:43 +0500

    config express app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import dotenv from "dotenv"
 import connectDb from "./db/index.js"
+import { app } from "./app.js"
 dotenv.config({
     path:'./env'
 })
-connectDb()
+
+connectDb().then(()=>{
+    app.listen(process.env.PORT || 5501 , ()=>{
+        console.log(`server is running at port ${process.env.PORT}`)
+    })
+}).catch((err)=>{
+    console.log("MONGODB connection error" ,err);
+})
---
+++ src/models/user.model.js
@@ -0,0 +1,71 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+    firstName:{
+        type:String,
+        required:true
+    },
+    lastName:{
+        type:String,
+        required:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        trim:true
+    },
+    password:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    userType:{
+        type:String,
+        required:true
+    },
+    role:{
+        type:String,
+        default:'admin'
+    },
+    profession:{
+        type:String,
+        required:true,
+    },
+    desc:{
+        type:String,
+        required:true,
+        min:[15],
+        max:[500]
+    },
+    profile_image:{
+        type:String,
+        default:'https://static.vecteezy.com/system/resources/previews/009/734/564/non_2x/default-avatar-profile-icon-of-social-media-user-vector.jpg'
+    },
+    gender:{
+        type:String,
+        required:true
+    },
+    address:{
+        city:String,
+        country:String,
+        zipCode:Number,
+        street:String
+
+    },
+    office:{
+        companyName:String,
+        companyWebsite:String,
+        totalEmployees:{
+            type:Number,
+            default:0
+        },
+        companyFoundedIn:{
+            type: Date,
+            default:Date.now()
+        }
+    }
+},{ timestamps: true } )
+
+const _userSchema = new mongoose.model("userSchema", userSchema , 'user_schema')
+export default _userSchema
