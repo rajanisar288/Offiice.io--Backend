d3d794f2da773b3843a082465f7b58f5cc69da05
rajanisar288 <raaji4194@gmail.com>
2024-01-11 18:57:43 +0500

    fix code spacing

--- src/middlewares/verifyAccessToken.js
+++ src/middlewares/verifyAccessToken.js
@@ -1,19 +1,28 @@
-import Jwt  from "jsonwebtoken";
-import _userSchema from "./../models/user.model.js"
-
+import Jwt from "jsonwebtoken";
+import _userSchema from "./../models/user.model.js";
 
 // Verify request if is it authorize!!
 const authenticateToken = (req, res, next) => {
-    const accessToken = req.header('Authorization') || req.cookie('accessToken');
-    if (!accessToken) return res.status(401).json({status:false,message:'Unauthorized request'});
-  
-    jwt.verify(accessToken, process.env.AccessTokenSecretKey, async (err, user) => {
+  const accessToken = req.header("Authorization") || req.cookie("accessToken");
+  if (!accessToken)
+    return res
+      .status(401)
+      .json({ status: false, message: "Unauthorized request" });
+
+  jwt.verify(
+    accessToken,
+    process.env.AccessTokenSecretKey,
+    async (err, user) => {
       if (err) return res.sendStatus(403);
-      const validUser = await _userSchema.findById(user._id)
-      if(!validUser) return res.status(401).json({status:false, message:'User is not valid'})
+      const validUser = await _userSchema.findById(user._id);
+      if (!validUser)
+        return res
+          .status(401)
+          .json({ status: false, message: "User is not valid" });
       req.user = validUser;
       next();
-    });
-  };
+    }
+  );
+};
 
-  export default authenticateToken
+export default authenticateToken;
